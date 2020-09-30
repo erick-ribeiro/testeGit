e524ddc6fae597563d0d22b7977965f595a55bd8
Erick Ribeiro <gustavoribeiroerick@gmail.com>
2020-09-30 09:28:43 -0300

    first commit

---
+++ testeGitProject.dpr
@@ -0,0 +1,14 @@
+program testeGitProject;
+
+uses
+  Vcl.Forms,
+  uTesteGit in 'uTesteGit.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
